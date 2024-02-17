3a14ed15573f88d748bf75df81884e7c5abac945
Ishaan1103 <ishaanrana2635@gmail.com>
2024-02-17 18:53:41 +0530

    App for Curd

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+const productRoute = require("./routes/product.route.js");
+const app = express();
+
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+//routes
+app.use("/api/products", productRoute);
+
+
+app.get('/', (req, res) => {
+    res.send("Hello for node API server");
+});
+
+
+
+mongoose.connect("mongodb+srv://ishaanrana2635:08SMuYlepUadrfE6@cluster0.1hdp4x2.mongodb.net/Node-API?retryWrites=true&w=majority")
+    .then(() => {
+        console.log('connected to DB');
+        app.listen(3000, () => {
+            console.log("server is listening at 3000 port");
+        });
+    })
+    .catch(() => {
+        console.log("Failed to connect");
+    })
---
+++ routes/product.route.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const Product = require("../models/product.model.js");
+const router = express.Router();
+const {getProducts,getProduct, createProduct, updateProduct, deleteProduct} = require('../controllers/product.controller.js');
+
+
+router.get('/', getProducts);
+router.get('/:id',getProduct);
+router.post('/',createProduct);
+
+//update
+
+router.put("/:id",updateProduct);
+
+//delete
+router.delete("/:id",deleteProduct);
+module.exports = router;
